b7045a7dc2cf9a6bfafe280659822164eaca6aeb
qwerty <145857108+rubakhaanastasia@users.noreply.github.com>
2024-11-02 14:20:38 +0100

    Add files via upload

---
+++ js/work.js
@@ -0,0 +1,43 @@
+1;
+const перевіритиПоля = function () {
+  const pole1 = prompt("pole1");
+  const pole2 = prompt("pole2");
+  if (pole1 && pole2) {
+    alert("Обидва поля заповнені");
+  } else {
+    alert("Не всі поля заповнені");
+  }
+};
+2;
+function перевіритиСуму() {
+  const num1 = parseFloat("num1");
+  const num2 = parseFloat("num2");
+  const сума = num1 + num2;
+  if (сума > 10) {
+    alert("Сума більша за 10");
+  } else {
+    alert("Сума менша або дорівнює 10");
+  }
+}
+3;
+const word = "JavaScript";
+const worrd = prompt("Введіть слово");
+if (includes("JavaScript")) {
+                alert("Текст містить слово JavaScript");
+            } else {
+                alert("Текст не містить слово JavaScript");
+}
+4          
+function перевіритиЧисло() {
+            const numberNum = parseFloat("число");
+            if (число > 10 && число < 20) {
+                alert("Число входить в діапазон від 10 до 20");
+            } else {
+                alert("Число не входить в діапазон від 10 до 20");
+            }
+}
+5
+// const name = prompt((не менше 3 символів));
+// const email = prompt((має містити '@' та '.'));
+// const password = prompt((не менше 6 символів));
+
