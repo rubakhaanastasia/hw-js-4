203c1f5047919fd5e462a03fef6d64d2c456d58e
qwerty <145857108+rubakhaanastasia@users.noreply.github.com>
2024-11-16 16:56:52 +0100

    Add files via upload

---
+++ js/work.js
@@ -0,0 +1,35 @@
+//1
+// const field = prompt("введіть значення для першого поля");
+// const fielde = prompt("введіть значення для другого поля");
+// field !== "" && fielde !== ""
+//   ? alert("обидва поля заповнені")
+//   : alert("не всі поля заповнені");
+
+//2
+// const stock = Number(prompt("введіть перше число"));
+// const stocke = Number(prompt("введіть друге число"));
+// if (sum > 10) {
+//   alert("сума більша за 10");
+// } else {
+//   alert("сума менша або дорівнює 10");
+// }
+//3
+// const text = prompt("введіть текст зі словом JavaScript");
+// if (text.includes("JavaScript")) {
+//   alert("текст містить слово JavaScript");
+// } else {
+//   alert("текст не містить слово JavaScript");
+// }
+//4
+// const num = Number(prompt("введіть число"));
+// if (num > 10 && num < 20) {
+//   alert("Число входить в діапазон від 10 до 20");
+// } else {
+//   alert("Число не входить в діапазон від 10 до 20");
+// }
+//5
+const name = prompt("введіть ім'я (не менше 3 символів)");
+const email = prompt(
+  "введіть email (email містить символ @ та крапку після неї):"
+);
+const password = prompt("введіть пароль (пароль містить не менше 6 символів):");
